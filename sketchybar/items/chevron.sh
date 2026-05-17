2c372f110005c1191682a55313775c2b344e6e2a
Tenyama <tsunlee2610@gmail.com>
2026-05-17 16:31:25 +0700

    clearrr

--- sketchybar/items/chevron.sh
+++
@@ -1,2 +0,0 @@
-sketchybar --add item chevron left \
-  --set chevron icon=">>" label.drawing=off icon.color=$COLOR_WHITE icon.font="Hack Nerd Font:Regular:16.0"
--- sketchybar/items/front_app.sh
+++
@@ -1,6 +0,0 @@
-sketchybar --add item front_app center \
-  --set front_app script="$PLUGIN_DIR/window_title.sh" \
-  label.font="Hack Nerd Font:Regular:14.0" \
-  padding_left=100 \
-  padding_right=100 \
-  --subscribe front_app front_app_switched
